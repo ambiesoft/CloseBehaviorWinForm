cf26f3d19c8a251f7a31fbe5be251c47ee5e8790
ambiesoft <ambiesoft.trueff@gmail.com>
2020-05-08 19:02:23 +0900

    nazo

--- CloseBehaviorWinForm/FormMain.h
+++ CloseBehaviorWinForm/FormMain.h
@@ -42,20 +42,21 @@ namespace CloseBehaviorWinForm {
 			}
 			delete sw_;
 		}
-	private: System::Windows::Forms::TextBox^  txtLogFile;
-	private: System::Windows::Forms::CheckBox^  chkPreventDefaultWM_ENDESSSION;
-	private: System::Windows::Forms::RadioButton^  rbReturnsZero;
-	private: System::Windows::Forms::RadioButton^  rbReturnsOne;
+	private: System::Windows::Forms::TextBox^ txtLogFile;
+	private: System::Windows::Forms::CheckBox^ chkPreventDefaultWM_ENDESSSION;
+	private: System::Windows::Forms::RadioButton^ rbReturnsZero;
+	private: System::Windows::Forms::RadioButton^ rbReturnsOne;
+	private: System::Windows::Forms::Label^ lblLogFile;
 
-	protected: 
+	protected:
 
-	protected: 
+	protected:
 
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -68,22 +69,24 @@ namespace CloseBehaviorWinForm {
 			this->chkPreventDefaultWM_ENDESSSION = (gcnew System::Windows::Forms::CheckBox());
 			this->rbReturnsZero = (gcnew System::Windows::Forms::RadioButton());
 			this->rbReturnsOne = (gcnew System::Windows::Forms::RadioButton());
+			this->lblLogFile = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// txtLogFile
 			// 
-			this->txtLogFile->Location = System::Drawing::Point(28, 41);
+			this->txtLogFile->Location = System::Drawing::Point(15, 25);
 			this->txtLogFile->Name = L"txtLogFile";
+			this->txtLogFile->ReadOnly = true;
 			this->txtLogFile->Size = System::Drawing::Size(539, 20);
-			this->txtLogFile->TabIndex = 0;
+			this->txtLogFile->TabIndex = 200;
 			// 
 			// chkPreventDefaultWM_ENDESSSION
 			// 
 			this->chkPreventDefaultWM_ENDESSSION->AutoSize = true;
-			this->chkPreventDefaultWM_ENDESSSION->Location = System::Drawing::Point(28, 67);
+			this->chkPreventDefaultWM_ENDESSSION->Location = System::Drawing::Point(15, 51);
 			this->chkPreventDefaultWM_ENDESSSION->Name = L"chkPreventDefaultWM_ENDESSSION";
 			this->chkPreventDefaultWM_ENDESSSION->Size = System::Drawing::Size(199, 17);
-			this->chkPreventDefaultWM_ENDESSSION->TabIndex = 2;
+			this->chkPreventDefaultWM_ENDESSSION->TabIndex = 300;
 			this->chkPreventDefaultWM_ENDESSSION->Text = L"Prevent Default WM_ENDESSSION";
 			this->chkPreventDefaultWM_ENDESSSION->UseVisualStyleBackColor = true;
 			this->chkPreventDefaultWM_ENDESSSION->CheckedChanged += gcnew System::EventHandler(this, &FormMain::chkPreventDefaultWM_ENDESSSION_CheckedChanged);
@@ -92,10 +95,10 @@ namespace CloseBehaviorWinForm {
 			// 
 			this->rbReturnsZero->AutoSize = true;
 			this->rbReturnsZero->Checked = true;
-			this->rbReturnsZero->Location = System::Drawing::Point(64, 90);
+			this->rbReturnsZero->Location = System::Drawing::Point(38, 74);
 			this->rbReturnsZero->Name = L"rbReturnsZero";
 			this->rbReturnsZero->Size = System::Drawing::Size(66, 17);
-			this->rbReturnsZero->TabIndex = 3;
+			this->rbReturnsZero->TabIndex = 400;
 			this->rbReturnsZero->TabStop = true;
 			this->rbReturnsZero->Text = L"returns 0";
 			this->rbReturnsZero->UseVisualStyleBackColor = true;
@@ -103,27 +106,37 @@ namespace CloseBehaviorWinForm {
 			// rbReturnsOne
 			// 
 			this->rbReturnsOne->AutoSize = true;
-			this->rbReturnsOne->Location = System::Drawing::Point(64, 113);
+			this->rbReturnsOne->Location = System::Drawing::Point(38, 97);
 			this->rbReturnsOne->Name = L"rbReturnsOne";
 			this->rbReturnsOne->Size = System::Drawing::Size(66, 17);
-			this->rbReturnsOne->TabIndex = 4;
+			this->rbReturnsOne->TabIndex = 500;
 			this->rbReturnsOne->Text = L"returns 1";
 			this->rbReturnsOne->UseVisualStyleBackColor = true;
+			// 
+			// lblLogFile
+			// 
+			this->lblLogFile->AutoSize = true;
+			this->lblLogFile->Location = System::Drawing::Point(12, 9);
+			this->lblLogFile->Name = L"lblLogFile";
+			this->lblLogFile->Size = System::Drawing::Size(47, 13);
+			this->lblLogFile->TabIndex = 100;
+			this->lblLogFile->Text = L"Log File:";
 			// 
 			// FormMain
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(621, 283);
+			this->Controls->Add(this->lblLogFile);
 			this->Controls->Add(this->rbReturnsOne);
 			this->Controls->Add(this->rbReturnsZero);
 			this->Controls->Add(this->chkPreventDefaultWM_ENDESSSION);
 			this->Controls->Add(this->txtLogFile);
 			this->Name = L"FormMain";
 			this->Text = L"FormMain";
-			this->Load += gcnew System::EventHandler(this, &FormMain::FormMain_Load);
-			this->FormClosed += gcnew System::Windows::Forms::FormClosedEventHandler(this, &FormMain::FormMain_FormClosed);
 			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &FormMain::FormMain_FormClosing);
+			this->FormClosed += gcnew System::Windows::Forms::FormClosedEventHandler(this, &FormMain::FormMain_FormClosed);
+			this->Load += gcnew System::EventHandler(this, &FormMain::FormMain_Load);
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
@@ -151,15 +164,15 @@ namespace CloseBehaviorWinForm {
 			{
 			case WM_QUERYENDSESSION:
 				WriteLog("WM_QUERYENDSESSION");
-				if(chkPreventDefaultWM_ENDESSSION->Checked)
+				if (chkPreventDefaultWM_ENDESSSION->Checked)
 				{
-					if(rbReturnsZero->Checked)
+					if (rbReturnsZero->Checked)
 					{
 						WriteLog("At WM_QUERYENDSESSION returns 0");
 						m.Result = (IntPtr)0;
 						return;
 					}
-					if(rbReturnsOne->Checked)
+					if (rbReturnsOne->Checked)
 					{
 						WriteLog("At WM_QUERYENDSESSION returns 1");
 						m.Result = (IntPtr)1;
@@ -173,30 +186,32 @@ namespace CloseBehaviorWinForm {
 			}
 			Form::WndProc(m);
 		}
-	private: System::Void FormMain_Load(System::Object^  sender, System::EventArgs^  e) {
-				 UpdateDialog();
-				 String^ logFile = Application::ExecutablePath + ".txt";
-				 txtLogFile->Text = logFile;
+	private:
+		System::Void FormMain_Load(System::Object^ sender, System::EventArgs^ e) {
+			UpdateDialog();
+			String^ logFile = Path::Combine(Path::GetDirectoryName(Application::ExecutablePath),
+				Path::GetFileNameWithoutExtension(Application::ExecutablePath) + ".txt");
+			txtLogFile->Text = logFile;
 
-				 sw_ = gcnew StreamWriter(logFile, true);
-				 sw_->WriteLine();
-				 WriteLog("Started");
+			sw_ = gcnew StreamWriter(logFile, true);
+			sw_->WriteLine();
+			WriteLog("Started");
 
-			 }
-	private: System::Void FormMain_FormClosing(System::Object^  sender, System::Windows::Forms::FormClosingEventArgs^  e) {
-				 WriteLog("FormMain_FormClosing at");
-			 }
-	private: System::Void FormMain_FormClosed(System::Object^  sender, System::Windows::Forms::FormClosedEventArgs^  e) {
-				 sw_->WriteLine("FormMain_FormClosed at " + NowString);
-			 }
-			 void UpdateDialog()
-			 {
-				 rbReturnsZero->Enabled = chkPreventDefaultWM_ENDESSSION->Checked;
-				 rbReturnsOne->Enabled = chkPreventDefaultWM_ENDESSSION->Checked;
-			 }
-	private: System::Void chkPreventDefaultWM_ENDESSSION_CheckedChanged(System::Object^  sender, System::EventArgs^  e) {
-				 UpdateDialog();
-			 }
-};
+		}
+		System::Void FormMain_FormClosing(System::Object^ sender, System::Windows::Forms::FormClosingEventArgs^ e) {
+			WriteLog("FormMain_FormClosing at");
+		}
+		System::Void FormMain_FormClosed(System::Object^ sender, System::Windows::Forms::FormClosedEventArgs^ e) {
+			sw_->WriteLine("FormMain_FormClosed at " + NowString);
+		}
+		void UpdateDialog()
+		{
+			rbReturnsZero->Enabled = chkPreventDefaultWM_ENDESSSION->Checked;
+			rbReturnsOne->Enabled = chkPreventDefaultWM_ENDESSSION->Checked;
+		}
+		System::Void chkPreventDefaultWM_ENDESSSION_CheckedChanged(System::Object^ sender, System::EventArgs^ e) {
+			UpdateDialog();
+		}
+	};
 }
 
